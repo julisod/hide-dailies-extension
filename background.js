4e100783bce0a83127869bddccf45607f16c4e7c
Julia Söderlund <julia.soderlund@yandex.ru>
2025-07-17 17:04:42 +0300

    🎨 Fix formatting with Prettier

--- background.js
+++ background.js
@@ -6,25 +6,25 @@ chrome.action.onClicked.addListener(async (tab) => {
     // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
     // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+    const nextState = prevState === "ON" ? "OFF" : "ON";
 
     // Set the action badge to the next state
     await chrome.action.setBadgeText({
       tabId: tab.id,
-      text: nextState
+      text: nextState,
     });
 
     if (nextState === "ON") {
       // Insert the CSS file when the user turns the extension on
       await chrome.scripting.insertCSS({
         files: ["no-dailies.css"],
-        target: { tabId: tab.id }
+        target: { tabId: tab.id },
       });
     } else if (nextState === "OFF") {
       // Remove the CSS file when the user turns the extension off
       await chrome.scripting.removeCSS({
         files: ["no-dailies.css"],
-        target: { tabId: tab.id }
+        target: { tabId: tab.id },
       });
     }
   }
--- firefox/background.js
+++ firefox/background.js
@@ -1,26 +1,28 @@
-const habiticaURL = "https://habitica.com"
+const habiticaURL = "https://habitica.com";
 
 const handleClick = async (tab) => {
   if (tab.url.startsWith(habiticaURL)) {
     // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await browser.browserAction.getBadgeText({tabId: tab.id})
+    const prevState = await browser.browserAction.getBadgeText({
+      tabId: tab.id,
+    });
     // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+    const nextState = prevState === "ON" ? "OFF" : "ON";
 
-    browser.browserAction.setBadgeText({text: nextState, tabId: tab.id})
+    browser.browserAction.setBadgeText({ text: nextState, tabId: tab.id });
 
     if (nextState === "ON") {
-        // Insert the CSS file when the user turns the extension on
-        browser.tabs.insertCSS(tab.id, {
-          file: "no-dailies.css"
-        });
-      } else if (nextState === "OFF") {
-        // Remove the CSS file when the user turns the extension off
-        browser.tabs.removeCSS(tab.id, {
-          file: "no-dailies.css"
-        });
-      }
+      // Insert the CSS file when the user turns the extension on
+      browser.tabs.insertCSS(tab.id, {
+        file: "no-dailies.css",
+      });
+    } else if (nextState === "OFF") {
+      // Remove the CSS file when the user turns the extension off
+      browser.tabs.removeCSS(tab.id, {
+        file: "no-dailies.css",
+      });
     }
-}
+  }
+};
 
 browser.browserAction.onClicked.addListener(handleClick);
